a88f0943435b710b2b574caebe851fbac1605d2c
Sajti Zsolt Zoltán <sajtizsolt@gmail.com>
2021-12-10 21:15:39 +0100

    Simple commands

--- source/commandparser.py
+++ source/commandparser.py
@@ -11,23 +11,15 @@ class CommandParser:
 
   def verify_command(self, command):
     command_parts = command.split(" ")
-    command_parts_length = len(command_parts)
-    if command_parts_length < 2:
-      print_help_and_exit(Messages.TOO_SHORT_COMMAND)
-    elif command_parts_length > 7:
-      print_help_and_exit(Messages.TOO_LONG_COMMAND)
+    if len(command_parts) != 2:
+      print_help_and_exit(Messages.INVALID_COMMAND)
     self.verify_team_name(command_parts[0])
-    self.verify_member_names(command_parts[1 : command_parts_length])
+    self.verify_member_name(command_parts[1])
 
   def verify_team_name(self, team_name):
     if not re.fullmatch(self.REGEX_TEAM, team_name):
       print_help_and_exit(Messages.INVALID_TEAM_NAME)
 
-  def verify_member_names(self, member_names):
-    verified_names = set()
-    for name in member_names:
-      if not re.fullmatch(self.REGEX_PERSON,name):
-        print_help_and_exit(Messages.INVALID_MEMBER_NAME)
-      if name in verified_names:
-        print_help_and_exit(Messages.MEMBER_NAME_DUPLICATE)
-      verified_names.add(name)
+  def verify_member_name(self, member_name):
+    if not re.fullmatch(self.REGEX_PERSON, member_name):
+      print_help_and_exit(Messages.INVALID_MEMBER_NAME)
--- source/main.py
+++ source/main.py
@@ -9,8 +9,8 @@ def main():
   argument_parser.verify_arguments(sys.argv)
   command_list = argument_parser.get_commands(sys.argv)
   database = Database()
-  for raw_command in command_list:
-    database.execute_command(Command(raw_command))
+  for command_str in command_list:
+    database.execute_command(Command(command_str))
   database.print()
 
 if __name__ == '__main__':
